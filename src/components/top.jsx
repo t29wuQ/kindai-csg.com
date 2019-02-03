fdf51ee1d25d329eab820ff44ce1b22d3c60cc08
K.Tsukimoto <t.29.wuq@gmail.com>
2019-02-03 14:34:39 +0900

    :sparkles: heightを動的に変更

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -5,19 +5,24 @@ import Circle from './circle'
 
 const TopStyle = styled.div`
     background-color: yellow;
+    height: ${props => props.height}px;
 `;
 
 class Top extends Component{
     constructor(props){
         super(props);
         this.state={
-            width: 0
+            width: 0,
+            height: 0
         }
     }
     render(){
         return(
-            <TopStyle>
-                <EventListener target="window" onResize={() => this.setState({width: window.innerWidth})}/>
+            <TopStyle height={this.state.height}>
+                <EventListener target="window" onResize={() => {
+                    this.setState({width: window.innerWidth});
+                    this.setState({height: window.innerHeight});
+                }}/>
                 {this.state.width}
                 <Circle width={
                     (() => {
